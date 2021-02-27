3111ce13a21229689f2b172da8520c6141d0e077
Peter S <ptrsuc@gmail.com>
2021-02-27 09:04:14 +0000

    Added authentication to the application

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,45 @@
 package dev.psuchanek
 
+import dev.psuchanek.database.checkPasswordForEmail
+import dev.psuchanek.routes.loginRoute
+import dev.psuchanek.routes.registerRoute
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+    install(Authentication) {
+        configureAuth()
+    }
+    install(Routing) {
+        registerRoute()
+        loginRoute()
+    }
+}
+
+private fun Authentication.Configuration.configureAuth() {
+    basic {
+        realm = "Logbook Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email)
+            } else null
+        }
+    }
 }
 
